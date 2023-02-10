a76e877931a36649648516b4e81d7963b31f43a8
CristobalTorres <cristobal.torres6771@gmail.com>
2023-02-10 17:01:12 -0500

    archivos

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "AGUJERO";
+const accessCode2 = "LLAVE";
+const accessCode3 = "BASE DE DATOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text(" G E J U R A O", 100,50)
+    fill("lightblue")
+    text("Pista: ¡Qué cosa es que cuanto más le quitas más grande es!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("L E L A V", 700,150)
+    fill("lightblue")
+    text("Pista: ¡No muerde ni ladra, pero tiene dientes y la casa guarda!", 550,170)
+
+    fill("white")
+    textSize(15)
+    text("B S D D T S A E E A O", 100,250)
+    fill("lightblue")
+    text("Pista: ¡Almacena toda la información!", 100,270)
+
+}
